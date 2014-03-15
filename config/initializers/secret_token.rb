689b8ea2a72ad18fc772837d8695b1343e8f351a
DeVon McWhite <dmcwhite@gmail.com>
2014-03-14 17:32:42 -0800

    Full push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yadda::Application.config.secret_key_base = 'b61d5574a709ed4285ce088c8414268f89837c87995e77435f4254b01e850ac37029fbdeac7a67286b73ee85b8e20b2626083362a6dde499e216e4af462d4faf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yadda::Application.config.session_store :cookie_store, key: '_yadda_session'
